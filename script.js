a03da850f87a08c9694e6034cdf550a0be6d56ad
Joe <joetkane@gmail.com>
2024-01-18 16:13:25 +0000

    more stuff

--- script.js
+++ script.js
@@ -23,21 +23,40 @@
 //   result.innerHTML = Math.floor(Math.random() * 6) + 1;
 // };
 
-const weather = "sunny";
-const clothing = document.getElementById("clothing");
+// const weather = "sunny";
+// const clothing = document.getElementById("clothing");
 
-const whatToWear = (weather) => {
-  switch (weather) {
-    case "sunny":
-      clothing.innerHTML = "use sunscreen";
-      break;
+// const whatToWear = (weather) => {
+//   switch (weather) {
+//     case "sunny":
+//       clothing.innerHTML = "use sunscreen";
+//       break;
 
-    case "windy":
-      clothing.innerHTML = "do not bring an umbrella";
-      break;
+//     case "windy":
+//       clothing.innerHTML = "do not bring an umbrella";
+//       break;
 
-    case "rainy":
-      clothing.innerHTML = " PLEASE bring an umbrella";
-      break;
+//     case "rainy":
+//       clothing.innerHTML = " PLEASE bring an umbrella";
+//       break;
+//   }
+// };
+
+const myInterest = document.querySelector(".my_interest");
+
+const calculateInterest = () => {
+  let i = parseFloat(document.querySelector('input[name="savings"]').value);
+  while (i < 2000) {
+    i = i * 1.28;
+    myInterest.innerHTML += `<li>${i}</li>`;
+    if (i > 2000) break;
   }
 };
+
+// codewars bashing
+
+function duplicateCount(text){
+  for (let i = 0; i < text.length; i++) {
+       text[i] ===
+  }
+}
